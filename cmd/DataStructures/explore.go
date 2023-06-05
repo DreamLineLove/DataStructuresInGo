7ee01a4b29b593f7466e5fd050668b7612b6caee
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-05 20:18:32 +0630

    Got more experience working with go maps

--- cmd/DataStructures/explore.go
+++ cmd/DataStructures/explore.go
@@ -3,22 +3,28 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 )
+
+type Person struct {
+	nickname   string
+	birthmonth time.Month
+}
 
 func explore() {
 	littleStringManipulationExercise()
 }
 
 func littleStringManipulationExercise() {
-	purposes := map[string]string{
+	stringsFunctions := map[string]string{
 		"Contains":     "Returns true or false whether a string contains a substring",
 		"ContainsAny":  "Returns true or false whether a string contains any of the characters from the substring",
 		"ContainsRune": "Returns true or false whether a string contains a rune",
 	}
 
-	functions := make([]string, 3)
+	functions := make([]string, len(stringsFunctions))
 	i := 0
-	for key := range purposes {
+	for key := range stringsFunctions {
 		functions[i] = key
 		i++
 	}
@@ -30,10 +36,37 @@ func littleStringManipulationExercise() {
 	fmt.Println(intro + functionsNewLine + ".\n")
 
 	instructions := make([]string, 0)
-	for _, value := range purposes {
+	for _, value := range stringsFunctions {
 		instructions = append(instructions, value)
 	}
 	for _, value := range instructions {
 		fmt.Println(value)
 	}
+	fmt.Println()
+
+	leute := make([]Person, 2)
+	leute[0] = Person{
+		nickname:   "Ko Arkar",
+		birthmonth: time.February,
+	}
+	leute[1] = Person{
+		nickname:   "Tr Ei Phyo",
+		birthmonth: time.January,
+	}
+
+	goetheStudentsBirthmonths := make(map[string]time.Month, 2)
+	for _, value := range leute {
+		goetheStudentsBirthmonths[value.nickname] = value.birthmonth
+	}
+	fmt.Println(goetheStudentsBirthmonths)
+	fmt.Println()
+
+	value, exists := goetheStudentsBirthmonths["Ko Toe"]
+	if exists {
+		fmt.Println(value)
+	} else {
+		fmt.Println("Die Information von Ko Toe does not exist!")
+	}
+	goetheStudentsBirthmonths["Ko Arkar"] = time.April
+
 }
