010449c8d23ec08111a01bb980f66d97e64acfe5
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-05 17:57:25 +0630

    You should NOT use the bracket syntax (name[i]) with append() to append to slices AT ALL. You should only use the spread/expansion syntax (...) in append(). Also, the appender MUST be a SLICE, arrays simply aren't accepted!

--- cmd/DataStructures/explore.go
+++ cmd/DataStructures/explore.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func explore() {
 	var countries [3]string
@@ -25,4 +28,48 @@ func explore() {
 	all = append(all, secondaries...)
 	fmt.Printf("%q\n\n", all[0:6])
 
+	abcdef := [6]rune{
+		'a',
+		'b',
+		'c',
+		'd',
+		'e',
+		'f',
+	}
+	abc := abcdef[0:3]
+	def := abcdef[3:6]
+	fmt.Println(abc, def)
+	abc = append(abc, def...)
+	fmt.Println(abc)
+
+	luxury_brands := make([]string, 2, 5)
+	luxury_brands[0] = "GUCCI"
+	luxury_brands[1] = "DIOR"
+	luxury_brands = append(luxury_brands, "Louis Vuitton")
+	other_luxury_brands := []string{
+		"ARMANI EXCHANGE",
+		"Chanel",
+	}
+	armani_only, _ := strings.CutSuffix(other_luxury_brands[0], "EXCHANGE")
+	armani_final := strings.TrimSuffix(armani_only, " ")
+	luxury_brands = append(luxury_brands, armani_final, other_luxury_brands[1])
+	var temp string
+	temp = luxury_brands[2]
+	luxury_brands[2] = luxury_brands[3]
+	luxury_brands[3] = temp
+	fmt.Printf("%q\n", luxury_brands)
+
+	fmt.Println()
+	simpleSlice := []float32{
+		6.99,
+		10.49,
+	}
+	floatArray := []float32{
+		11.99,
+		4.99,
+		5.99,
+	}
+	fmt.Println(simpleSlice)
+	simpleSlice = append(simpleSlice, floatArray...)
+	fmt.Println(simpleSlice)
 }
